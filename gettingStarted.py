f15f71d719513f3891544bd441c1b45313d1fe20
Smit Kashyap Trivedi <112817199+st4972@users.noreply.github.com>
2023-01-28 12:48:52 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -6,9 +6,9 @@ def welcome_assignment_answers(question):
     # Students do not have to follow the skeleton for this assignment.
     # Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "Yes"
-    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
         answer = "No"
+    elif question == "Is it possible to decrypt a message without a key? - Yes/No":
+        answer = "Yes"
     elif question == "Is it possible to decode a message without a key? - Yes/No":
         answer = "No"
     elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
