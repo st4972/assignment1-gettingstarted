5470037bd7e099592fbfc9f4157628559d8ece87
Smit Kashyap Trivedi <112817199+st4972@users.noreply.github.com>
2023-01-28 11:39:20 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -3,33 +3,52 @@
 ### Output - The right answer for the specific question.
 
 def welcome_assignment_answers(question):
-    #Students do not have to follow the skeleton for this assignment.
-    #Another way to implement is using a "case" statements similar to C.
+    # Students do not have to follow the skeleton for this assignment.
+    # Another way to implement is using a "case" statements similar to C.
     if question == "Are encoding and encryption the same? - Yes/No":
-        answer = "The student should type the answer here"
+        answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "The student should type the answer here"
-    else: 
+        answer = "No"
+    elif question == "Is it possible to decode a message without a key? - Yes/No":
+        answer = "No"
+    elif question == "Is a hashed message supposed to be un-hashed? - Yes/No":
+        answer = "No"
+    elif question == "What is the SHA256 hashing value to the following message: 'NYU Computer Networking'":
+        answer = "883C13DA6A24949C9A23231B60119E2ACE58459DA4F8BBDD812CC37764548BDD"
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
+        answer = "No"
+    elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an " \
+                     "integer number":
+        answer = "7"
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an " \
+                     "integer number":
+        answer = "3"
+    elif question == "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel " \
+                     "posted by a TA?":
+        answer = ""
+    else:
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
         ### maybe put something here to flag an issue
         answer = ""
-    return(answer)
+    return (answer)
+
+
 # Complete all the questions.
 
 
 if __name__ == "__main__":
-    #use this space to debug and verify that the program works
+    # use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
 
-#Questions:
-#"In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
-#"Are encoding and encryption the same? - Yes/No":
-#"Is it possible to decrypt a message without a key? - Yes/No":
-#"Is it possible to decode a message without a key? - Yes/No":
-#"Is a hashed message supposed to be un-hashed? - Yes/No":
-#"What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
-#"Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
-#"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+# Questions:
+# "In Slack, what is the secret passphrase posted in the #lab-python-getting-started channel posted by a TA?":
+# "Are encoding and encryption the same? - Yes/No":
+# "Is it possible to decrypt a message without a key? - Yes/No":
+# "Is it possible to decode a message without a key? - Yes/No":
+# "Is a hashed message supposed to be un-hashed? - Yes/No":
+# "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
+# "Is MD5 a secured hashing algorithm? - Yes/No":
+# "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+# "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
